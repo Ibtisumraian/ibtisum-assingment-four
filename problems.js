469d57bd9755346273d0bd1dcfa020f998629ef6
Ibtisumraian <ibtisumraian@gmail.com>
2025-02-18 19:52:01 +0600

    emty commit

--- problems.js
+++ problems.js
@@ -120,29 +120,30 @@
 
 
 
-let  person1 = { name: "Rahul", gender: "male", age: 28 };
- let person2 = { name: "Joya", gender: "female", age: 21 };
+// let  person1 = { name: "Rahul", gender: "male", age: 28 };
+//  let person2 = { name: "Joya", gender: "female", age: 21 };
  
 
 
 
 
-function validProposal( person1 , person2){
+// function validProposal( person1 , person2){
 
-    if(typeof person1 !== 'object' || typeof person2 !== 'object'){
-        return 'invalid'
-    }
+//     if( typeof person1 !== 'object' ||
+//         typeof person2 !== 'object'){
+//         return 'invalid'
+//     }
 
-    // let ageD = Math.abs(person1.age - person2.age);
     
-    if( person1.gender !== person2.gender && 
-        Math.abs(person1.age - person2.age) === 7){
-        return "true"
-    }else{ 
-        return "false"
-    }
+    
+//     if( person1.gender !== person2.gender && 
+//         Math.abs(person1.age - person2.age) === 7){
+//         return "true"
+//     }else{ 
+//         return "false"
+//     }
 
-}
+// }
 
-const result = validProposal( person1, person2)
-console.log(result)
+// const result = validProposal( person1, person2)
+// console.log(result)
